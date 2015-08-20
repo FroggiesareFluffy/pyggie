4e635efe20e5ce50d91a14cdaba1ef62d43f03a8
FroggiesareFluffy <jhia186034@adams12.org>
2015-08-20 17:31:20 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Game(pygame.sprite.Group):
+    def __init__(self):
+        pygame.sprite.Group.__init__(self)
+        self.mobiles = []
+        self.damagers = []
+        self.statics = []
+        self.was_updated = True
+
+    def add_internal(self,s):
+        if isinstance(s,Damager):
+            self.damagers.append(s)
+        elif isinstance(s,Static):
+            self.statics.append(s)
+        elif isinstance(s,(Controlled,PathFollower)):
+            self.mobiles.append(s)
+        Game.add_internal(self,s)
+        self.was_updated = True
+
+    def remove_internal(self,s):
+        if isinstance(s,Damager):
+            self.damagers.remove(s)
+        elif isinstance(s,Static):
+            self.statics.remove(s)
+        elif isinstance(s,(Controlled,PathFollower)):
+            self.mobiles.remove(s)
+        Game.remove_internal(self,s)
+        self.was_updated = True
+
+    def update(self):
+        for s in self.sprites():
+            s.update()
+        self.was_updated = False
+
+from mobiles import PathFollower,Controlled
+from statics import Static
+from bases import Damager
